053d0ac449f4bfdb576452b3903f9c616e68f569
Clinxi <jsha0707@163.com>
2023-04-13 14:33:36 +0800

    predefine

--- src/ast.hpp
+++ src/ast.hpp
@@ -26,6 +26,72 @@ class CompUnitAST : public BaseAST {
   virtual ~CompUnitAST() override{};
 };
 
+/*
+class Decl : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~Decl() override {}
+};
+*/
+
+/*
+class ConstDel : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~ConstDel() override {}
+};
+*/
+
+/*
+class BType : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~BType() override {}
+};
+*/
+
+/*
+class ConstDef : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~ConstDef() override {}
+};
+*/
+
+/*
+class ConstInitVal : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~ConstInitVal() override {}
+};
+*/
+
+/*
+class VarDecl : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~VarDecl() override {}
+};
+*/
+
+/*
+class VarDef : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~VarDef() override {}
+};
+*/
+
+/*
+class InitVal : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~InitVal() override {}
+};
+*/
+
+
+
 class FuncDefAST : public BaseAST {
  public:
   std::unique_ptr<BaseAST> func_type;
@@ -46,12 +112,38 @@ class FuncType : public BaseAST {
   virtual ~FuncType() override {}
 };
 
+/*
+class FuncFParams : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~FuncFParams() override {}
+};
+*/
+
+/*
+class FuncFParam : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~FuncFParam() override {}
+};
+*/
+
+
 class Block : public BaseAST {
  public:
   std::unique_ptr<BaseAST> stmt;
   virtual void printTree() override;
   virtual ~Block() override {}
 };
+
+/*
+class BlockItem : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~BlockItem() override {}
+};
+*/
+
 
 class Stmt : public BaseAST {
  public:
@@ -60,6 +152,30 @@ class Stmt : public BaseAST {
   virtual ~Stmt() override {}
 };
 
+/*
+class Exp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~Exp() override {}
+};
+*/
+
+/*
+class LVal : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~LVal() override {}
+};
+*/
+
+/*
+class PrimaryExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~LVal() override {}
+};
+*/
+
 class Number : public BaseAST {
  public:
   int content;
@@ -67,5 +183,82 @@ class Number : public BaseAST {
   virtual ~Number() override {}
 };
 
+/*
+class UnaryExp : public BaseAST {
 
+  virtual void printTree() override;
+  virtual ~UnaryExp() override {}
+};
+*/
+
+/*
+class UnaryOp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~UnaryOp() override {}
+};
+*/
+/*
+class FuncRParams : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~FuncRParams() override {}
+};
+*/
+
+/*
+class MulExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~MulExp() override {}
+};
+*/
+
+/*
+class AddExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~AddExp() override {}
+};
+*/
+
+/*
+class RelExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~RelExp() override {}
+};
+*/
+
+/*
+class EqExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~EqExp() override {}
+};
+*/
+
+/*
+class LAndExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~LAndExp() override {}
+};
+*/
+
+/*
+class LOrExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~LOrExp() override {}
+};
+*/
+
+/*
+class ConstExp : public BaseAST {
+
+  virtual void printTree() override;
+  virtual ~ConstExp() override {}
+};
+*/
 #endif
