910a42656868e1304b7ff52ea4aceac18b809d25
JSC1375 <565405659@qq.com>
2023-05-01 22:29:11 +0800

    add a pdf

--- input.c
+++ input.c
@@ -1,18 +1,29 @@
 
 void f(int a) { int b; }
-void quick_sort_recursive(int start, int end) {
+void quick_sort_recursive(int start, int end)
+{
   int mid;
   int left;
   left = mid;
-  if (left + mid) {
-    if (left - mid) {
+  if (left + mid)
+  {
+    if (left - mid)
+    {
       left = left + mid;
-    } else {
+    }
+    else
+    {
       mid = mid + 1;
       mid = mid + 1;
     }
     left = left + 1;
-  } else {
+  }
+  else
+  {
     left = left - 1;
   }
+}
+int main()
+{
+  return 0;
 }
