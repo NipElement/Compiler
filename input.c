732a9605166f4a9db0cc37a2c07600ff5f184f83
JSC1375 <565405659@qq.com>
2023-05-01 16:27:02 +0800

    add input.c

---
+++ input.c
@@ -0,0 +1,18 @@
+
+void f(int a) { int b; }
+void quick_sort_recursive(int start, int end) {
+  int mid;
+  int left;
+  left = mid;
+  if (left + mid) {
+    if (left - mid) {
+      left = left + mid;
+    } else {
+      mid = mid + 1;
+      mid = mid + 1;
+    }
+    left = left + 1;
+  } else {
+    left = left - 1;
+  }
+}
