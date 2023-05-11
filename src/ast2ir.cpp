67fd52d4655cc1c67091258fa4414f5203487327
JSC1375 <565405659@qq.com>
2023-05-11 20:50:39 +0800

    modify switch case  to if else

--- src/ast2ir.cpp
+++ src/ast2ir.cpp
@@ -485,58 +485,47 @@ BaseIr *EqExpAST::buildIrTree() {
 }
 
 BaseIr *RelExpAST::buildIrTree() {
-  switch (rel_exp_rule) {
-    // AddExp
-    case 0:
-      return add_exp_ast->buildIrTree();
-      break;
-    // RelExp LT AddExp
-    case 1:
-      auto binop = new BinopExp();
+  if (rel_exp_rule == 0) {  // AddExp
+    return add_exp_ast->buildIrTree();
+  } else if (rel_exp_rule == 1) {  // RelExp LT AddExp
+    auto binop = new BinopExp();
 
-      binop->op = BinOpType(Lt);
-      binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
-      binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
-      binop->bool_result_reg = reg++;
-      binop->reg_id = reg++;
+    binop->op = BinOpType(Lt);
+    binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
+    binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
+    binop->bool_result_reg = reg++;
+    binop->reg_id = reg++;
 
-      return binop;
-      break;
-    // RelExp GT AddExp
-    case 2:
-      auto binop = new BinopExp();
+    return binop;
+  } else if (rel_exp_rule == 2) {  // RelExp GT AddExp
+    auto binop = new BinopExp();
 
-      binop->op = BinOpType(Gt);
-      binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
-      binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
-      binop->bool_result_reg = reg++;
-      binop->reg_id = reg++;
-      return binop;
-      break;
-    // RelExp LE AddExp
-    case 3:
-      auto binop = new BinopExp();
+    binop->op = BinOpType(Gt);
+    binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
+    binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
+    binop->bool_result_reg = reg++;
+    binop->reg_id = reg++;
+    return binop;
+  } else if (rel_exp_rule == 3) {  // RelExp LE AddExp
+    auto binop = new BinopExp();
 
-      binop->op = BinOpType(Le);
-      binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
-      binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
-      binop->bool_result_reg = reg++;
-      binop->reg_id = reg++;
-      return binop;
-      break;
-    // RelExp GE AddExp
-    case 4:
-      auto binop = new BinopExp();
+    binop->op = BinOpType(Le);
+    binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
+    binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
+    binop->bool_result_reg = reg++;
+    binop->reg_id = reg++;
+    return binop;
+  } else if (rel_exp_rule == 4) {  // RelExp GE AddExp
+    auto binop = new BinopExp();
 
-      binop->op = BinOpType(Ge);
-      binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
-      binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
-      binop->bool_result_reg = reg++;
-      binop->reg_id = reg++;
-      return binop;
-      break;
-    default:
-      break;
+    binop->op = BinOpType(Ge);
+    binop->exp1 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(rel_exp_ast->buildIrTree()));
+    binop->exp2 = std::unique_ptr<ExpIr>(dynamic_cast<ExpIr *>(add_exp_ast->buildIrTree()));
+    binop->bool_result_reg = reg++;
+    binop->reg_id = reg++;
+    return binop;
+  } else {
+    return nullptr;
   }
 }
 
