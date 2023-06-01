e6be3498682f51917771c0af2a4f601d03d7c930
JSC1375 <565405659@qq.com>
2023-06-01 23:40:40 +0800

    Update ir2ll.cpp

--- src/ir2ll.cpp
+++ src/ir2ll.cpp
@@ -311,8 +311,13 @@ void NorBoolBinopExp::printLL() {
       ConstExp *theConstExp = dynamic_cast<ConstExp *>(exp2.get());
       // %bool_result_reg = icmp eq i32 %exp2->reg_id, const
       // %reg_id = zext i1 %bool_result_reg to i32
-      cout << "  %" << bool_result_reg << " = icmp eq i32 %" << exp1->reg_id << ", " << theConstExp->value << endl;
-      cout << "  %" << reg_id << " = zext i1 %" << bool_result_reg << " to i32" << endl;
+      if (exp1->res_type == VariableType(Char)) {
+        cout << "  %" << bool_result_reg << " = icmp eq i8 %" << exp1->reg_id << ", " << theConstExp->value << endl;
+        cout << "  %" << reg_id << " = zext i1 %" << bool_result_reg << " to i32" << endl;
+      } else {
+        cout << "  %" << bool_result_reg << " = icmp eq i32 %" << exp1->reg_id << ", " << theConstExp->value << endl;
+        cout << "  %" << reg_id << " = zext i1 %" << bool_result_reg << " to i32" << endl;
+      }
     }
   } else if (op == Ne) {
     if (constFlag == 0) {
